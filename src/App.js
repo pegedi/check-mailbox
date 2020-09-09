f43fa21212f7956acfa038c117f4ff1400095c81
pegedi <peter.egedi@gmail.com>
2020-09-09 13:23:15 +0000

    devMode:True works and funxtion executed!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import CustomizedTables from './Table';
 //import jsonp from './jsonp';
 import downloadfile2 from './downloadToFile';
 import StatusBar from './StatusBar';
-//import {API_KEY_ENV, CLIENT_ID_ENV} from './.env.local'
 
 const queryURL='https://script.google.com/macros/s/AKfycbzNEIVgweOKPUyS9rjAOePMG2fTcKy1YIj0V8cI_VpMTGQLuA3-/exec?query=label:orareport';
 const queryDEVURL='https://script.google.com/macros/s/AKfycbymuFfnEq2Rw-KSq93_3u4qpKnFiOhQMn-uY2_3IdMo/dev?query=label:orareport';
@@ -15,7 +14,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID; 
 const DISCOVERY_DOCS = ["https://script.googleapis.com/$discovery/rest?version=v1"];
-var SCOPES = 'https://www.googleapis.com/auth/script.projects';
+var SCOPES = 'https://www.googleapis.com/auth/script.projects https://www.googleapis.com/auth/drive';
 
 
 const headerCells = [{fieldName: "Report Name", align: "left"},
@@ -110,10 +109,87 @@ function App({appUName, setAppUName}) {
       event.stopPropagation();
    };
 
+    function callAppsScript() {
+        window.gapi.client.script.projects.create({
+            resource: {
+                title: 'My Script'
+            }
+        }).then((resp) => {
+            return window.gapi.client.script.projects.updateContent({
+                scriptId: resp.result.scriptId,
+                resource: {
+                    files: [{
+                        name: 'hello',
+                        type: 'SERVER_JS',
+                        source: 'function helloWorld() {\n  console.log("Hello, world!");\n}'
+                        }, {
+                        name: 'appsscript',
+                        type: 'JSON',
+                        source: "{\"timeZone\":\"America/New_York\",\"" +
+                        "exceptionLogging\":\"CLOUD\"}"
+                    }]
+                }
+            });
+        }).then((resp) => {
+          let result = resp.result;
+          if (result.error) throw result.error;
+          console.log(`https://script.google.com/d/${result.scriptId}/edit`);
+        }).catch((error) => {
+          // The API encountered a problem.
+          return console.log(`The API returned an error: ${error}`);
+        });
+      }
+
+   function callScriptFunction0() {
+       const scriptId = '1_LCQKiOadsTyDbwJIGwN0jwyS9xJ3z-wDq_Pgk2P-wtpbJGX1EObhntl';
+       console.log(window.gapi);
+       let auth = window.gapi.auth2.getAuthInstance();
+       // Call the Apps Script API run method
+       //   'scriptId' is the URL parameter that states what script to run
+       //   'resource' describes the run request body (with the function name
+       //              to execute)
+
+       window.gapi.client.script.scripts.run({
+           'scriptId': scriptId,
+           'resource': {
+               'function': 'firstAPIFunction',
+               "devMode": true
+            }
+       }).then(function(resp){
+           let result=resp.result;
+           if (result.error && result.error.status) {
+               // The API encountered a problem before the script started executing.
+               console.log('Error calling API:');
+               console.log(result);
+           } else if (result.error) {
+               // The API executed, but the script returned an error.
+
+               // Extract the first (and only) set of error details.
+               // The values of this object are the script's 'errorMessage' and
+               // 'errorType', and an array of stack trace elements.
+               let error = result.error.details[0];
+               console.log(result.error);
+               console.log('Script error message: ' + error.errorMessage);
+
+               if (error.scriptStackTraceElements) {
+                   console.log('Script error stacktrace:')
+                   for (const element of error.scriptStackTraceElements) {
+                       console.log(element);
+                   }
+               }
+           } else {
+               // The structure of the result will depend upon what the Apps
+               console.log(resp);
+           }
+       })
+
+   }
    const inquiryPressed = (event) => {
     console.log('Refresh Started!');
     setAppUName('-ha');
     console.log(appUName);
+//    callAppsScript();
+    callScriptFunction0();
 //    jsonp(queryDEVURL, response => {setDataRows(response)});
  
   };
